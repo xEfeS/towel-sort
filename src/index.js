2e1be98438f6e08579eeab7a366f80cbdb7e1d7b
xEfes <artiom.kruk.97@mail.ru>
2020-10-19 19:34:29 +0300

    feat:add towelSort solotion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (matrix == undefined) {
+        return [];
+    };
+
+    return matrix.reduce((result, row, i) => {
+        if ((i + 1) % 2 == 0) {
+            return result.concat(row.reverse());
+        } else {
+            return result.concat(row);
+        }
+    }, []);
+};
